5f14fcc61e09001b6247f493bd41d5bc5a20e9d0
Daniel-McNamee <S00273249@atu.ie>
2026-01-28 13:20:20 +0000

    added logic to prevent duplicates

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,23 +10,50 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('Worksheet2');
 
-    movies: string[] = [
-    'Inception',
-    'The Matrix',
-    'Interstellar',
-    'Gladiator',
-    'The Dark Knight'
+  // Populate movies array
+  movies: string[] = [
+  'Inception',
+  'The Matrix',
+  'Interstellar',
+  'Gladiator',
+  'The Dark Knight',
+  'The Shawshank Redemption',
+  'Pulp Fiction',
+  'Forrest Gump',
+  'The Godfather',
+  'Spirited Away',
+  'Fight Club',
+  'The Lord of the Rings'
   ];
 
+
+  // Create empty array for favourites
     favourites: string[] = [];
 
-  addToFavourites(movie: string) {
+addToFavourites(movie: string) {
+  // Add to favourites
   this.favourites.push(movie);
+
+  // Given string so must find index using indexOf
+  const index = this.movies.indexOf(movie);
+
+  // If indexOf cannot find the index it returns -1
+  if (index !== -1) {
+    // Remove from movies list
+    this.movies.splice(index, 1);
+  }
 }
 
 deleteFavourite(index: number) {
+  // Here we already know the index as it is given as a parameter
+  const movie = this.favourites[index];
+
+  // Add to movies list
+  this.movies.push(movie);
+  // Remove from favourites
   this.favourites.splice(index, 1);
 }
+
 
 
 }
